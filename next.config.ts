4533c4d634a7a55b80a031a10ec55fb4f24a9517
matomitw <matomi-lucky.ezekiel@teamwork.net>
2025-12-23 12:02:12 +0100

    next config modify

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   output: 'export',
-  /* config options here */
-    images: {
-    // Add the quality value (85) from your component
+  output: "export",
+
+  images: {
+    unoptimized: true,
     qualities: [85],
-    // You can also specify a range, for example:
-    // qualities: [75, 80, 85, 90, 95, 100],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
